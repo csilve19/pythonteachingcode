fb2a3c987159feafe2b58e0d67ff0bd230dffd6f
csilve19 <csilve19@students.kennesaw.edu>
2024-02-29 12:04:37 -0500

    correcting

--- Python Fundamentals/P2M1CameronSilveraRobinson.py
+++ Python Fundamentals/P2M1CameronSilveraRobinson.py
@@ -1,7 +1,7 @@
 # [] create words after "G" following the Assignment requirements use of functions, menhods and kwyowrds
 # Sample quote: "Wheresoever you go, go with all your heart" ~ Confucius (551 BC - 479 BC)
 
-qoute = input("Enter a famous saying: ")
+qoute = input("Welcome, Cameron Silvera-Robinson. \nEnter a 1 sentence quote, non-alpha separate words: ")
 word = ""
 
 for character in qoute:
